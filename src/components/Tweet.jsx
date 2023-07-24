d40740cc3d3af9bf50b450f5d0f508a07b32aa7f
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2023-07-25 00:10:16 +0200

    passing props into component

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,22 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+function Tweet(props) {
+  // console.log(props);
+  const tweet = props.tweet; // extract tweet
   return (
     <div className="tweet">
-      <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
-        className="profile"
-        alt="profile"
-      />
-
+      <ProfileImage image={props.tweet.user.image} />
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <Timestamp time={props.tweet.timestamp} />
         </div>
-
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Message message={props.tweet.message} />
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
